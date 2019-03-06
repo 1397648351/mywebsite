3bb2fed274cc7444cacb49b0ff008d9dcb09fef5
1397648351 <1397648351@qq.com>
2019-03-06 21:32:45 +0800

    1

--- public/static/dist/js/my.js
+++ public/static/dist/js/my.js
@@ -1,7 +1,7 @@
 ;!function (window) {
     "use strict";
     var myjs = function () {
-        // this
+        this.pjax_first = true;
     };
     myjs.prototype.init = function () {
         var th = this, progress = $('.progress');
@@ -16,37 +16,67 @@
         var ele = $(e);
         if (ele.length <= 0) return;
         var progress = ele.find('.progress-bar');
-        if (typeof (v) == 'undefined')
+        if (typeof v == 'undefined')
             v = progress.data('percent');
-        if (typeof (v) == 'undefined') v = 0;
-        if (typeof (v) == 'number')
+        if (typeof v == 'undefined') v = 0;
+        if (typeof v == 'number')
             progress.css('width', v + "%");
         else if (v.indexOf('%') > -1 || v.indexOf('px') > -1)
             progress.css('width', v);
     };
-    myjs.prototype.pjax = function () {
-        var ele = $("#page_progress");
-        var bar = $("#page_progress .progress-bar").get(0);
+    myjs.prototype.pjax = function (pin) {
+        var options = $.extend(true, {
+            element: null,
+            bar: '.progress-bar',
+            loadingClass: 'is-loading',
+            startCallback: null,
+            sendCallback: null,
+            clickedCallback: null,
+            beforeReplaceCallback: null,
+            completeCallback: null,
+        }, pin || {});
+        if (!options.element) return;
+        var ele = $(options.element);
+        if (!ele) return;
+        var bar = ele.find(options.bar).get(0);
         if (!bar) return;
         $(document).pjax('[data-pjax] a, a[data-pjax]', '#pjax-container');
         $(document).on('pjax:start', function () {
-            my.progress('#page_progress', 0);
+            my.progress(options.element, 0);
             bar.clientWidth;
-            ele.addClass('is-loading');
+            ele.addClass(options.loadingClass);
+            if(options.startCallback && typeof options.startCallback === "function"){
+                options.startCallback();
+            }
         });
         $(document).on('pjax:send', function () {
-            my.progress('#page_progress', 15);
+            my.progress(options.element, 15);
+            if(options.sendCallback && typeof options.sendCallback === "function"){
+                options.sendCallback();
+            }
         });
         $(document).on('pjax:clicked', function (event) {
-            document.title = 'bitms-' + event.target.innerHTML;
-            my.progress('#page_progress', 30);
+            document.title = event.target.innerHTML;
+            my.progress(options.element, 30);
+            if(options.clickedCallback && typeof options.clickedCallback === "function"){
+                options.clickedCallback();
+            }
         });
         $(document).on('pjax:beforeReplace', function () {
-            my.progress('#page_progress', 80);
+            my.progress(options.element, 80);
+            if(options.beforeReplaceCallback && typeof options.beforeReplaceCallback === "function"){
+                options.beforeReplaceCallback();
+            }
         });
         $(document).on('pjax:complete', function () {
-            my.progress('#page_progress', 100);
-            $("#page_progress").removeClass('is-loading');
+            my.progress(options.element, 100);
+            ele.removeClass(options.loadingClass);
+            $(document).on('pjax:beforeReplace', function () {
+                my.progress(options.element, 80);
+                if(options.completeCallback && typeof options.completeCallback === "function"){
+                    options.completeCallback();
+                }
+            });
         });
         $(document).on('pjax:popstate', function (event) {
             console.log(event);
